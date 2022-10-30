6e89268d82b117c8191b2e2fa169e4e01b45d069
fakanbiomowunmi@gmail.com <fakanbiomowunmi@gmail.com>
2022-10-30 10:52:23 -0400

    CoinFlip

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React from 'react'
+import FlipCoin from './Components/FlipCoin';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <FlipCoin />
+        
+    
+    </div>
+  );
+}
+
+export default App;
